84224cfc72b6df15b5e781a09d4751b5adf3304d
jake <jake.corrales@unadeca.net>
2024-05-24 14:46:23 -0600

    subir repositorio local

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+// import './App.css'
+import Navbar
+    from "./components/Navbar";
+import Infoinput from "./components/Infoinput.jsx";
+import NestedGrid from "./components/Grid.jsx";
+
+export default function App() {
+  return (
+      <div>
+          <Navbar />
+          <p>hola todo bien y tu como estas yo estoy muy bien me alegro </p>
+        <Infoinput />
+
+          <NestedGrid />
+      </div>
+
+
+  );
+}
+
+
+
