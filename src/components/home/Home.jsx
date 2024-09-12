eab990dd21e63e227e1a63c27a9b8641c64acb00
jake <jake.corrales@unadeca.net>
2024-09-11 22:41:06 -0600

    Refactor image paths in Home component but problem in web said

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,63 +8,63 @@ export default function Inicio() {
 
         {
             title: "SistemaGestionLibros",
-            img: "src/assets/repositorio1.png",
+            img: "./dist/src/assets/repositorio1.png",
             fecha: "Nov 11, 2023",
             Link: "https://github.com/JakecorralesCespedes/proyecto-final-programacion-2.git",
         },
 
         {
             title: "Uber Eats",
-            img: "src/assets/repositorio2.png",
+            img: "./dist/src/assets/repositorio2.png",
             fecha: "April 10, 2024",
             Link: "https://github.com/JakecorralesCespedes/Proyecto_final.git"
         },
 
         {
             title: "Phonebook",
-            img: "src/assets/repositorio3.png",
+            img: "./dist/src/assets/repositorio3.png",
             fecha: "May 16, 2024",
             Link: "https://github.com/JakecorralesCespedes/clase_3.git"
         },
         {
             title: "Color Flipper",
-            img: "src/assets/repositorio4.png",
+            img: "./dist/src/assets/repositorio4.png",
             fecha: "May 22, 2024",
             Link: "https://github.com/JakecorralesCespedes/EC3.git"
         },
         {
             title: "Alert msg",
-            img: "src/assets/repositorio5.png",
+            img: "./dist/src/assets/repositorio5.png",
             fecha: "May 23, 2024",
             Link: "https://github.com/JakecorralesCespedes/clase4.git"
         },
         {
             title: "Primer perfil personal HTML",
-            img: "src/assets/repositorio6.png",
+            img: "./dist/src/assets/repositorio6.png",
             fecha: "May 12, 2024",
             Link: "https://github.com/JakecorralesCespedes/HTML_perfil_personal.git"
         },
         {
             title: "Primer perfil personal HTML y Css",
-            img: "src/assets/repositorio7.png",
+            img: "./dist/src/assets/repositorio7.png",
             fecha: "May 15, 2024",
             Link: "https://github.com/JakecorralesCespedes/HTML_perfil_personal/tree/perfil_css"
         },
         {
             title: "Calculadora con implementacion localStorage",
-            img: "src/assets/repositorio8.png",
+            img: "./dist/src/assets/repositorio8.png",
             fecha: "May 30, 2024",
             Link: "https://github.com/JakecorralesCespedes/miniproyecto1.git"
         },
         {
             title: "todo-app-api",
-            img: "src/assets/repositorio9.png",
+            img: "./dist/src/assets/repositorio9.png",
             fecha: "Jun 13, 2024",
             Link: "https://github.com/JakecorralesCespedes/todo-app-api.git"
         },
         {
             title: "Cambio de moneda conexion con api",
-            img: "src/assets/repositorio10.png",
+            img: "./dist/src/assets/repositorio10.png",
             fecha: "Jun 13, 2024",
             Link: "https://github.com/JakecorralesCespedes/EC6.git"
         },
